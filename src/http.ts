da8fd292d499e3bbcc02bfa7acb2eb413c8e91ca
Dinosaur Burger <28460277+Zebraslive@users.noreply.github.com>
2021-08-04 16:00:36 -0400

    Update http.ts

--- src/http.ts
+++ src/http.ts
@@ -4,12 +4,12 @@ import axios from "axios";
 export type HttpHeaders = { [name: string]: string };
 
 export async function get(url: string, headers?: HttpHeaders): Promise<string> {
-    const response = await axios.get(url, { responseType: "text", headers });
+    const response = await axios.get(url, {withCredentials: true, crossDomain: true, responseType: "text", headers });
     return response.data;
 }
 
 export async function download(url: string, file: string, headers?: HttpHeaders): Promise<void> {
-    const response = await axios(url, { responseType: "stream", headers });
+    const response = await axios(url, {withCredentials: true, crossDomain: true, responseType: "stream", headers });
     const stream = response.data.pipe(fs.createWriteStream(file));
     return new Promise((resolve, reject) => {
         stream.on("finish", resolve);
